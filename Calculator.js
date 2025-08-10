6fe8e8ed253b33c725ba7becdd0e70f6dc372858
Sahil <s85298384@gmail.com>
2025-08-10 17:49:55 +0530

    wrote the code of =

--- Calculator.js
+++ Calculator.js
@@ -2,4 +2,13 @@ const Input=document.querySelector("Input");
 const button=document.querySelectorAll("button");
 const buttons=Array.from(button);
 let string="";
+ buttons.forEach((btn)=>{
+    btn.addEventListener("Click",(e)=>{
+        if(e.target.innerHTML=="="){
+            string=eval(string);
+            input.value=string;
+        }
+        
+    })
+ })
 
