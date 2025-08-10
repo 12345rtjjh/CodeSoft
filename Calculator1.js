215d396f91865dded818ef39e3bf5114b56cf00a
Sahil <s85298384@gmail.com>
2025-08-10 18:19:22 +0530

    Ok

--- Calculator1.js
+++ Calculator1.js
@@ -2,14 +2,13 @@ const input=document.querySelector("#Input");
 const button=document.querySelectorAll("button");
 const buttons=Array.from(button);
 let string="";
-buttons.forEach((btn)=>{
+ buttons.forEach((btn)=>{
     btn.addEventListener("click",(e)=>{
         if(e.target.innerHTML=="="){
             string=eval(string);
             input.value=string;
-
         }
-        else if(e.target.innerHTML=="AC")
+         else if(e.target.innerHTML=="AC")
         {
             string="";
             input.value=string;
@@ -24,7 +23,8 @@ buttons.forEach((btn)=>{
         else{
             string+=e.target.innerHTML;
             input.value=string;
+            
         }
-
     })
-})
+ })
+
